3211d5b5714526e8cd78d130a91cbe04ebb4ee4b
Laika-Beats <laikabeatsdenver@gmail.com>
2020-12-15 12:36:24 -0700

    json

--- Local + Session Storage/app.js
+++ Local + Session Storage/app.js
@@ -1,4 +1,17 @@
-//local sotrage
+// //local sotrage
 
-localStorage.setItem("item", "feed the cat");
-localStorage.setItem("user", "Joshua");
+// localStorage.setItem("item", "feed the cat");
+// localStorage.setItem("user", "Joshua");
+
+// //getting stuff back
+// const user = localStorage.getItem("user");
+
+// console.log("user");
+
+const list = ["feed the cat", "pet the cat", "sing to the cat"];
+
+// JSON.stringify prints the list as an array instead of a continuous string
+localStorage.setItem("list", JSON.stringify(list));
+
+getList = JSON.parse(localStorage.getItem("list"));
+console.log(getList);
