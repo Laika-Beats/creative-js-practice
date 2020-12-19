febf6f7a14313f1c4dbf298626d1542a25bd1020
Laika-Beats <laikabeatsdenver@gmail.com>
2020-12-19 14:00:07 -0700

    callbacks and higher order functions

---
+++ Arrays/app.js
@@ -0,0 +1,6 @@
+// const videos = ["shredding the gnar", "coding some dope biz", "boolin"];
+
+//a higher order function is a function that takes in another function
+// videos.forEach(function (video) {
+//   console.log(video);
+// });
