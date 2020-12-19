d407bb08fedff2206c32904aff16d0d189138c9f
Laika-Beats <laikabeatsdenver@gmail.com>
2020-12-19 14:24:09 -0700

    map and filter

--- Arrays/app.js
+++ Arrays/app.js
@@ -1,6 +1,36 @@
-// const videos = ["shredding the gnar", "coding some dope biz", "boolin"];
-
 //a higher order function is a function that takes in another function
 // videos.forEach(function (video) {
 //   console.log(video);
 // });
+
+const videos = ["shredding the gnar", "coding some dope biz", "boolin"];
+
+const games = [
+  { Game: "Destiny", Rating: 9 },
+  { Game: "FFVII", Rating: 8 },
+  { Game: "God of War", Rating: 10 },
+  { Game: "Apex Legends", Rating: 8 },
+  { Game: "Monster Hunter", Rating: 7 },
+];
+
+//MAP
+// const newVideos = videos.map(function (video) {
+//   return video.toUpperCase();
+// });
+
+//FIND
+// const searchGnar = videos.find(function (video) {
+//   return video.includes("gnar");
+// });
+// console.log(searchGnar);
+
+//FILTER
+// const shortSearch = videos.filter(function (video) {
+//   return video.length < 20;
+// });
+
+// const lowRating = games.filter(function (game) {
+//   return game.Rating < 9;
+// });
+
+// console.log(lowRating);
