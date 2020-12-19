456833d5055cb1253dd3267c6cfe0b405526271e
Laika-Beats <laikabeatsdenver@gmail.com>
2020-12-19 13:34:21 -0700

    localStorage forEach

--- Solo/app.js
+++ Solo/app.js
@@ -3,15 +3,15 @@ const list = document.querySelector(".todo-list");
 const submit = document.querySelector(".submit-button");
 const input = document.querySelector(".todo-input");
 
-//Run the todoInput fucntion when the submit button is clicked
+//runs the displayStorage function when the page is loaded
+document.addEventListener("DOMContentLoaded", displayStorage);
+
+//runs the todoInput fucntion when the submit button is clicked
 submit.addEventListener("click", todoInput);
 
 //Add a new todo item to the DOM todo-list
 function todoInput(event) {
   event.preventDefault();
-
-  //Grabs the text typed in to the input field
-  const inputValue = input.value;
 
   //creates a new item for the todo-list
   let newTodo = document.createElement("ul");
@@ -19,12 +19,49 @@ function todoInput(event) {
   //adds a class to the new new ul item
   newTodo.classList.add("todo");
 
+  //saves the input value of the new todo in localStorage
+  todoStorage(input.value);
+
   //adds the text submitted in the input field
-  newTodo.innerText = inputValue;
+  newTodo.innerText = input.value;
 
   //appends the todo to the todo-list
   list.appendChild(newTodo);
 
   //clears text from input after new todo-list item is created
   input.value = "";
+}
+
+function todoStorage(todo) {
+  let todos;
+
+  //if there is nothing in localStorage display an empty array
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+
+    //if there are items in localStorage grab them and parse them in to an array
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  //adds new todos created with the todoInput function to the localStorage array
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function displayStorage() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  //grabs 'each' todo in the localStorage array and appends them to the todo-list
+  todos.forEach(function (todo) {
+    let newTodo = document.createElement("ul");
+    newTodo.classList.add("todo");
+    newTodo.innerText = todo;
+    list.appendChild(newTodo);
+  });
 }
