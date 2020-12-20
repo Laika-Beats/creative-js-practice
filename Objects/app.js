49470fd955ab0cf937d84a493585200b584d8efa
Laika-Beats <laikabeatsdenver@gmail.com>
2020-12-20 12:57:42 -0700

    bind, call, and apply

--- Objects/app.js
+++ Objects/app.js
@@ -1,16 +1,31 @@
 // keyword "this" is a pointer -> it refers to an object
 
-const user = {
-  name: "Joshua",
-  videos: ["html", "css", "js", "react"],
-  greet() {
-    console.log(`Hello there ${this.name}`);
+// const user = {
+//   name: "Joshua",
+//   videos: ["html", "css", "js", "react"],
+//   greet() {
+//     console.log(`Hello there ${this.name}`);
 
-    const getVideos = () => {
-      console.log(`You currently have ${this.videos.length} videos`);
-    };
-    getVideos();
+//     const getVideos = () => {
+//       console.log(`You currently have ${this.videos.length} videos`);
+//     };
+//     getVideos();
+//   },
+// };
+
+// user.greet();
+
+const person = {
+  firstName: "Joshua",
+  lastName: "Thompson",
+  getName() {
+    console.log(this.firstName + " " + this.lastName);
   },
 };
 
-user.greet();
+function registerUser() {
+  this.getName();
+}
+
+const register = registerUser.bind(person);
+register();
