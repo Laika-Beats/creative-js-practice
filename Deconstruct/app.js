5e26ed0c4f2493fd54409fbca4ae78d662346fee
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-04 19:41:29 -0700

    deconstructing arrays and objects

---
+++ Deconstruct/app.js
@@ -0,0 +1,31 @@
+// const items = ["banana", "chocolate noodle", "apple", "cheese"];
+
+// const [banana, choco, ...rest] = items;
+
+// console.log(choco);
+
+// const newItems = [...items, "more stuff", "whatup"];
+
+// console.log(newItemsd);
+
+const user = {
+  name: "Joshua",
+  photos: [1, 2, 3, 4],
+  age: 25,
+  analytics: {
+    subscribers: 25000,
+    videos: 150,
+  },
+};
+
+console.log(user);
+
+// const photos = user.photos;
+
+const { photos } = user;
+const { age } = user;
+const { analytics } = user;
+
+console.log(photos);
+console.log(age);
+console.log(analytics);
