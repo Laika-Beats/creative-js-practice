b010b93c9a7ebbc6206e76505cffececf0c854e3
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-04 19:45:24 -0700

    iffe

--- Deconstruct/app.js
+++ Deconstruct/app.js
@@ -8,24 +8,30 @@
 
 // console.log(newItemsd);
 
-const user = {
-  name: "Joshua",
-  photos: [1, 2, 3, 4],
-  age: 25,
-  analytics: {
-    subscribers: 25000,
-    videos: 150,
-  },
-};
+// const user = {
+//   name: "Joshua",
+//   photos: [1, 2, 3, 4],
+//   age: 25,
+//   analytics: {
+//     subscribers: 25000,
+//     videos: 150,
+//   },
+// };
 
-console.log(user);
+// console.log(user);
 
-// const photos = user.photos;
+// // const photos = user.photos;
 
-const { photos } = user;
-const { age } = user;
-const { analytics } = user;
+// const { photos } = user;
+// const { age } = user;
+// const { analytics } = user;
 
-console.log(photos);
-console.log(age);
-console.log(analytics);
+// console.log(photos);
+// console.log(age);
+// console.log(analytics);
+
+// IFFE
+
+(function hello() {
+  console.log("How are you?");
+})();
