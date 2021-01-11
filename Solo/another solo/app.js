fa10df169ef216ebce6eb66fb426fc0283acf8db
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-11 09:52:24 -0700

    storage

--- Solo/another solo/app.js
+++ Solo/another solo/app.js
@@ -1,3 +1,27 @@
 const todoList = document.querySelector(".todo-list");
 const input = document.querySelector(".user-input");
 const submitBtn = document.querySelector(".submit");
+
+submitBtn.addEventListener("click", newTodo);
+document.addEventListener("DOMContentLoaded", displayStorage);
+
+function newTodo() {
+  const userInput = input.value;
+  newTodoItem = document.createElement("li");
+  newTodoItem.classList.add("todo-item");
+  newTodoItem.innerText = userInput;
+  saveTodo(userInput);
+  todoList.appendChild(newTodoItem);
+  input.value = " ";
+}
+
+function saveTodo(userInput) {
+  let storageTodos;
+  if (localStorage.getItem("todos") === null) {
+    storageTodos = [];
+  } else {
+    storageTodos = JSON.parse(localStorage.getItem("todos"));
+  }
+  storageTodos.push(userInput);
+  localStorage.setItem("todos", JSON.stringify(storageTodos));
+}
