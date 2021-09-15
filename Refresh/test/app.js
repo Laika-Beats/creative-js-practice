8b130a2d5dc9521d1e6af92f739329107d028a6f
Laika-Beats <laikabeatsdenver@gmail.com>
2021-09-14 20:00:50 -0600

    if funct

--- Refresh/test/app.js
+++ Refresh/test/app.js
@@ -1,13 +1,25 @@
-function greet(name, age) {
-  console.log(
-    "Welcome to our website " + name + ", you are " + age + " years old."
-  );
+// function greet(name, age) {
+//   console.log(
+//     "Welcome to our website " + name + ", you are " + age + " years old."
+//   );
+// }
+
+// function signUp() {
+//   let name = prompt("What is your name?");
+//   let age = prompt("What is your age?");
+//   greet(name, age);
+// }
+
+// signUp();
+
+function max(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
 }
 
-function signUp() {
-  let name = prompt("What is your name?");
-  let age = prompt("What is your age?");
-  greet(name, age);
-}
+let inbox = max(300, 50);
 
-signUp();
+console.log(inbox);
