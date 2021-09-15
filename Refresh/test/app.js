8b1081085ab431cec51b723c2442f3127d462ba3
Laika-Beats <laikabeatsdenver@gmail.com>
2021-09-14 19:57:51 -0600

    funcitons within functions

--- Refresh/test/app.js
+++ Refresh/test/app.js
@@ -1,4 +1,13 @@
-function greet() {
-  let name = prompt("What is your name?");
-  console.log("Welcome to our website " + name);
+function greet(name, age) {
+  console.log(
+    "Welcome to our website " + name + ", you are " + age + " years old."
+  );
 }
+
+function signUp() {
+  let name = prompt("What is your name?");
+  let age = prompt("What is your age?");
+  greet(name, age);
+}
+
+signUp();
