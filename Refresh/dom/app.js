5601e8c6dc2626eb8344e45518f8236ca5607ee0
Laika-Beats <laikabeatsdenver@gmail.com>
2021-09-15 20:01:03 -0600

    dom

---
+++ Refresh/dom/app.js
@@ -0,0 +1,6 @@
+// const title = document.getElementsByTagName("h2");
+// const items = document.getElementsByClassName("items");
+// const submit = document.getElementById("submit");
+
+const title = document.querySelector("h2");
+const items = document.querySelectorAll(".item");
