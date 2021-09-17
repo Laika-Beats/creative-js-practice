ff096e86b518e92a367d6b6914435fa05ffa5f8a
Laika-Beats <laikabeatsdenver@gmail.com>
2021-09-17 15:39:46 -0600

    makin moves

--- Refresh/vanillaNoteApp/more practice/app.js
+++ Refresh/vanillaNoteApp/more practice/app.js
@@ -1,1 +1,15 @@
-console.log("TEST");
+const noteInput = document.querySelector("#noteInput");
+const inputButton = document.querySelector("#inputButton");
+const noteList = document.querySelector("#noteList");
+
+inputButton.addEventListener("click", submitNote);
+
+function submitNote(e) {
+  e.preventDefault();
+  console.log(noteInput.value);
+
+  let newNote = document.createElement("li");
+  newNote.classList.add("note");
+  newNote.innerText = noteInput.value;
+  noteList.appendChild(newNote);
+}
