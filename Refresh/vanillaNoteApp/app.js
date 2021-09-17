8a170f0611d81da989ebfd2ad143fd31da0367ec
Laika-Beats <laikabeatsdenver@gmail.com>
2021-09-17 15:27:35 -0600

    start

--- Refresh/vanillaNoteApp/app.js
+++ Refresh/vanillaNoteApp/app.js
@@ -1,5 +1,6 @@
 const noteInput = document.querySelector("#noteInput");
 const submitButton = document.querySelector("#submitButton");
+const deleteB = document.querySelector(".delete");
 
 const noteList = document.querySelector("#noteList");
 
@@ -20,8 +21,14 @@ function getNotes() {
     newItem.classList.add("note");
     newItem.innerText = note;
     noteList.appendChild(newItem);
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "delete";
+    deleteButton.classList.add("delete");
+    newItem.append(deleteButton);
   });
 }
+
+noteList.addEventListener("click", deleteNote);
 
 submitButton.addEventListener("click", function (e) {
   e.preventDefault(); //prevents page refresh when button clicked.
@@ -38,5 +45,29 @@ submitButton.addEventListener("click", function (e) {
   newItem.classList.add("note");
   newItem.innerText = noteInput.value;
   noteList.appendChild(newItem);
+  let deleteButton = document.createElement("button");
+  deleteButton.innerText = "delete";
+  deleteButton.classList.add("delete");
+  newItem.append(deleteButton);
+
   noteInput.value = " "; // clears input field after submit.
 });
+
+function deleteNote(e) {
+  e.stopPropagation();
+  const currentDelete = e.target;
+  if (currentDelete.classList[0] === "delete") {
+    const note = currentDelete.parentElement;
+    let todos = [];
+
+    if (localStorage.getItem("notes") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("notes"));
+    }
+    const deleteIndex = note.children[0].innerText;
+    todos.splice(todos.indexOf(deleteIndex), 1);
+    localStorage.setItem("notes", JSON.stringify(todos));
+  }
+  window.location.reload();
+}
