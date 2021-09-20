85cdc03f8d479cee95386ca5919cc2ef69263d17
Laika-Beats <laikabeatsdenver@gmail.com>
2021-09-20 16:42:26 -0600

    sorting

--- Refresh/arrays/array.js
+++ Refresh/arrays/array.js
@@ -1,6 +1,6 @@
-const videos = ["Music", "Comedy", "Fitness", "Pets"];
+// const videos = ["Music", "Comedy", "Fitness", "Pets"];
 
-videos.push("Clouds");
+// videos.push("Clouds");
 
 // videos.forEach((video) => {
 //   console.log(video);
@@ -12,8 +12,24 @@ videos.push("Clouds");
 
 // FIND
 
-const searchTutorial = videos.find((video) => {
-  return video.includes("Comedy");
-});
+// const searchTutorial = videos.find((video) => {
+//   return video.includes("Comedy");
+// });
 
-console.log(searchTutorial);
+// console.log(searchTutorial);
+
+// videos.splice(0, "Fitness");
+
+// console.log(videos);
+
+const items = ["banana", "orange", "apple", "mango"];
+const ratings = [5, 44, 67, 56, 88, 13, 1, 4];
+
+ratings.sort((a, b) => a - b);
+
+console.log(ratings);
+
+items.sort((item1, item2) => {
+  return item1 > item2 ? 1 : -1;
+});
+console.log(items);
