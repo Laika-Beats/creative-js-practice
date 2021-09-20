d9e0c7fc42ef61a12ac4cd1cb5609f609fbae283
Laika-Beats <laikabeatsdenver@gmail.com>
2021-09-20 14:42:46 -0600

    filter and find

--- Refresh/arrays/array.js
+++ Refresh/arrays/array.js
@@ -2,6 +2,18 @@ const videos = ["Music", "Comedy", "Fitness", "Pets"];
 
 videos.push("Clouds");
 
-videos.forEach((video) => {
-  console.log(video);
+// videos.forEach((video) => {
+//   console.log(video);
+// });
+
+// const newVideos = videos.map((video) => {
+//   console.log(video.toUpperCase());
+// });
+
+// FIND
+
+const searchTutorial = videos.find((video) => {
+  return video.includes("Comedy");
 });
+
+console.log(searchTutorial);
